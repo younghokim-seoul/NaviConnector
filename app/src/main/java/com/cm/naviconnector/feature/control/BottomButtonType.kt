f6cf09674256bab9ee3af6b5f7e61d6700028d25
hyuna <hyuna9787@golfzon.com>
2025-09-24 22:09:56 +0900

    BottomButtonType 리소스 추가

--- app/src/main/java/com/cm/naviconnector/feature/control/BottomButtonType.kt
+++ app/src/main/java/com/cm/naviconnector/feature/control/BottomButtonType.kt
@@ -3,7 +3,13 @@ package com.cm.naviconnector.feature.control
 import androidx.annotation.DrawableRes
 import com.cm.naviconnector.R
 
-enum class BottomButtonType(@get:DrawableRes override val icon: Int) : ControlButton {
-    PLAY(R.drawable.play),
-    PAUSE(R.drawable.pause)
+enum class BottomButtonType(
+    @get:DrawableRes val enabledIcon: Int,
+    @get:DrawableRes val disabledIcon: Int
+) : ControlButton {
+    PLAY(R.drawable.play_enable, R.drawable.play_disable),
+    PAUSE(R.drawable.pause_enable, R.drawable.pause_disable);
+
+    override val icon: Int
+        get() = enabledIcon
 }
